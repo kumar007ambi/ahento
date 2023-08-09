66503d55e24a355f04e3e6647c3a8303e9e5b3c1
kumar007ambi <60788663+kumar007ambi@users.noreply.github.com>
2023-08-09 04:28:06 -0400

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +10,7 @@ import { PageNotFoundComponent } from './pageNotFound/pageNotFound.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     HomeComponent,
---
+++ src/app/reactiveForm/reactiveForm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reactiveForm-app',
+  templateUrl: './reactiveForm.component.html',
+  // styleUrls: ['./app.component.css']
+})
+export class ReactiveFormComponent {
+  genders = ['male', 'female'];
+}
