73c10c9db271854648d27ec8b73c6db342709cbf
kumar007ambi <60788663+kumar007ambi@users.noreply.github.com>
2023-08-09 05:53:38 -0400

    1 file modified

--- src/app/reactiveForm/reactiveForm.component.ts
+++ src/app/reactiveForm/reactiveForm.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'reactiveForm-app',
   templateUrl: './reactiveForm.component.html',
   // styleUrls: ['./app.component.css']
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup | undefined;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl('male'),
+    });
+  }
 }
